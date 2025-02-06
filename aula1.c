9725e7ddf9ca432d803da47166fc05fc618b6abc
VitoriaCanon <vitoriacanon1212@gmail.com>
2025-02-06 14:31:58 -0300

    Estudos C

---
+++ aula1.c
@@ -0,0 +1,11 @@
+//Variáveis e printf 
+
+#include <stdio.h> //área de diretiva do pré-processador 
+//stdio.h - Standard I O (In and Out)
+
+int main() {
+    int idade
+
+    printf("Hello, World!\n");
+    return 0;
+}
